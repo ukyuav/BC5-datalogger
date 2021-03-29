066e63bb1b4ad576cad7eacc6a269ee2f06550c6
Isaac Rowe <irowe.bbn@gmail.com>
2021-03-29 15:19:07 -0400

    Update VectorNav DAQ combo script

--- vn_daq_formatter.py
+++ vn_daq_formatter.py
@@ -15,21 +15,21 @@ if __name__ == "__main__":
         with open(vn_string) as vn_file:
             vn_reader = csv.DictReader(vn_file, delimiter=",")
             first_row = next(vn_reader)
-            first_sync = int(first_row.get("SyncInCnt", False))
+            first_sync = int(first_row.get("SyncIn Count", False))
             if not first_sync:
                 print("ERROR: cannot combine files without SYNCINCNT being recorded on VNDATA")
                 quit()
             else:
                 with open("vndaq_combined.csv", "w") as outfile:
                     outwriter = csv.writer(outfile, delimiter=",")
-                    outwriter.writerow(vn_reader.fieldnames + ["DAQ1", "DAQ2", "DAQ3", "DAQ4", "DAQ5", "DAQ6", "DAQ7", "DAQ8"])
+                    outwriter.writerow(vn_reader.fieldnames + ["DAQTIME", "DAQ1", "DAQ2", "DAQ3", "DAQ4", "DAQ5", "DAQ6", "DAQ7", "DAQ8"])
                     last_sync = 0
                     current_sync = int(first_sync)
                     outwriter.writerow([l for l in first_row.values()] + next(daq_reader)) # need to write the first row of things to get us started
                     try:
                         for row in vn_reader:
                             last_sync = int(current_sync) # the most recent recorded sync
-                            current_sync = int(row["SyncInCnt"]) # the sync we are trying to catch up to
+                            current_sync = int(row["SyncIn Count"]) # the sync we are trying to catch up to
                             row_diff =  current_sync - last_sync # the difference between where we are vs. where we were in the daq samples
                             if row_diff < 1:
                                 continue # skip row
