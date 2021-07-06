fe64d5b259d5bc9643b6d3220c883e959406b263
Josh Ashley <jashley2017@gmail.com>
2021-07-06 18:07:18 +0100

    committing iMET and USB code

---
+++ iMET_logging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "launching python script\n"
+sleep 1
+sudo python3 iMET_sampling.py /media/sda1/ &
+sleep 1
+echo "end of logger.sh"
---
+++ usb_disconnect.py
@@ -0,0 +1,21 @@
+#Purpose: check usb connections every so often and output a message to LCD screen if USB-drive ever disconnects
+
+import os
+import time
+import serial
+import sys
+import subprocess 
+
+
+while True:
+#os.system("lsusb")
+    sub = subprocess.Popen("lsusb", shell=True, stdout=subprocess.PIPE)
+    
+    time.sleep(0.05)
+    line = sub.stdout.read().decode()
+    time.sleep(0.05)
+    if "SanDisk" not in line:
+        exit()
+    else:
+        time.sleep(0.01)
+
