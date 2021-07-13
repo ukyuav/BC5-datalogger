8542780002f6cf8e2f02d80270f59519f296e35b
Jared Gibson <jareda.gibson@uky.edu>
2021-07-13 18:56:23 +0100

    adding iMET and USB code

--- iMET_logging.sh
+++ iMET_logging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "launching python script\n"
+echo "launching iMET python script\n"
 sleep 1
-sudo python3 iMET_sampling.py /media/sda1/ &
+sudo python3 iMET_sampling.py /home/pi/BC6B &
 sleep 1
 echo "end of logger.sh"
